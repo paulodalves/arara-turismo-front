e0dab8678bf727c09c9fda4606063400e99b187a
paulodalves <paulodalves95@gmail.com>
2022-03-23 02:03:15 -0300

    primeiro commit

--- src/services/comentario.service.js
+++ src/services/comentario.service.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:3000/api/destino";
+const API_URL = process.env.REACT_APP_API_URL + "/api/destino";
 
 const getAll = id => {
     return axios.get(API_URL + "/detalhes/" + id + "/comentarios");
--- src/services/destino.service.js
+++ src/services/destino.service.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:3000/api/mapa";
+const API_URL = process.env.REACT_APP_API_URL + "/api/mapa";
 
 const getAll = () => {
     return axios.get(API_URL + "/destinos");
